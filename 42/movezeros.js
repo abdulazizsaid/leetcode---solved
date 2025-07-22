621f0a9d47c4f1eaa78e0b0b9bed265915dd7a9b
abdulazizsaidovcode <abdulazizsaidovcode@gmail.com>
2025-07-22 15:31:56 +0500

    go go

---
+++ 42/movezeros.js
@@ -0,0 +1,13 @@
+function moveZeroes(nums){
+    for (let index = 0; index < nums.length; index++ ) {
+
+        if((nums[index] == 0) && nums[index + 1]) {
+            nums[index] 
+            nums[index] = a
+
+        }
+    }
+   return nums
+}
+
+console.log(moveZeroes([0,1,0,3,12]));
